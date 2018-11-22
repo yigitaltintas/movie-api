6591146c919ae0d88db1071ce3d6ce3e81d9e85a
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2018-11-22 20:49:36 +0300

    proje dosyalari olusturuldu.Film ekleme endpointi yazildi.

---
+++ helper/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+  mongoose.connect('mongodb://mongo_user:Mongo1234@ds029635.mlab.com:29635/movie-api', {  useNewUrlParser: true });
+
+  mongoose.connection.on('open', () => {
+     console.log('MongoDB: Connected');
+  });
+
+  mongoose.connection.on('error', (err) => {
+     console.log(err.errmsg);
+  });
+
+  mongoose.Promise = global.Promise;
+};
