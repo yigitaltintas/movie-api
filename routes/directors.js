04a5088d70ff77cb1d1917dc95c101cf55013b20
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2018-11-24 02:04:22 +0300

    Yonetmeleri listemele endpointi yazildi.

--- routes/directors.js
+++ routes/directors.js
@@ -16,4 +16,51 @@ router.post('/', (req, res) => {
     })
 });
 
+/** Get Directors */
+router.get('/', (req, res) => {
+   const director = Director.aggregate([
+       {
+           $lookup : {
+               from : 'movies',
+               localField: '_id',
+               foreignField : 'director_id',
+               as : 'movies'
+           }
+       },
+       {
+           $unwind: {
+               path : '$movies',
+               preserveNullAndEmptyArrays : true
+           }
+       },
+       {
+           $group: {
+               _id : {
+                   _id : '$_id',
+                   name : '$name',
+                   surname : '$surname',
+                   bio : '$bio'
+               },
+               movies : {
+                   $push : '$movies'
+               }
+           }
+       },
+       {
+           $project : {
+               _id : '$_id._id',
+               name : '$_id.name',
+               surname : '$_id.surname',
+               movies : '$movies'
+           }
+       }
+   ]);
+
+   director.then( data => {
+       res.json(data);
+   }).catch( err => {
+       res.json(err);
+   })
+});
+
 module.exports = router;
