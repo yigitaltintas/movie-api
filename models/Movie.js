43c5126f097803e89ad067b8fda12b49a1922572
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2018-11-24 17:48:35 +0300

    Movie validasyon duzenlemesi yapildi.

--- models/Movie.js
+++ models/Movie.js
@@ -6,11 +6,25 @@ const MovieSchema = new Schema({
     director_id : Schema.Types.ObjectId,
     title : {
         type : String,
-        required : true
+        required : [true, '`{PATH}` alani zorunludur.'],
+        maxlength : [60, '`{PATH}` alani (`{MAXLENGTH}`)  karakterden buyuk olamaz.'],
+        minlength : [5, '`{PATH}` alani (`{MINLENGTH}`) karakterden kucuk olamaz.']
     },
-    category : String,
-    country : String,
-    year : Number,
+    category : {
+        type : String,
+        maxlength: 30,
+        minlength: 2
+    },
+    country : {
+        type : String,
+        maxlength : 30,
+        minlength : 2
+    },
+    year : {
+        type : Number,
+        maxlength : 2025,
+        minlength : 1900
+    },
     imdb_score : Number,
     createdAt : {
         type : Date,
