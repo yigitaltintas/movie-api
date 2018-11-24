88b9c5eff22528af7c1d8938ca326710f90bcbd4
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2018-11-24 17:18:08 +0300

    Tek yonetmen bilgisine ulasma endpointi yazildi.

--- routes/directors.js
+++ routes/directors.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router  = express.Router();
 
@@ -61,6 +62,58 @@ router.get('/', (req, res) => {
    }).catch( err => {
        res.json(err);
    })
+});
+
+/** Get One Director */
+router.get('/:director_id', (req, res) => {
+    const director = Director.aggregate([
+        {
+          $match : {
+              '_id' : mongoose.Types.ObjectId(req.params.director_id)
+          }
+        },
+        {
+            $lookup : {
+                from : 'movies',
+                localField: '_id',
+                foreignField : 'director_id',
+                as : 'movies'
+            }
+        },
+        {
+            $unwind: {
+                path : '$movies',
+                preserveNullAndEmptyArrays : true
+            }
+        },
+        {
+            $group: {
+                _id : {
+                    _id : '$_id',
+                    name : '$name',
+                    surname : '$surname',
+                    bio : '$bio'
+                },
+                movies : {
+                    $push : '$movies'
+                }
+            }
+        },
+        {
+            $project : {
+                _id : '$_id._id',
+                name : '$_id.name',
+                surname : '$_id.surname',
+                movies : '$movies'
+            }
+        }
+    ]);
+
+    director.then( data => {
+        res.json(data);
+    }).catch( err => {
+        res.json(err);
+    })
 });
 
 module.exports = router;
