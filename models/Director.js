507d71623a1062c2a46a0f2f723505430e1fed60
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2018-11-24 17:49:40 +0300

    Director validasyon duzenlemesi yapildi.

--- models/Director.js
+++ models/Director.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-   name : String,
+   name : {
+       type : String,
+       required : true,
+       maxlength : 60,
+       minlength : 2
+   },
    surname : String,
    bio : String,
    createdAt : {
