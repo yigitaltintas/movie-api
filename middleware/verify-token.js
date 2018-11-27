debd7b47220045fe4d4d3222e8cb995399d49661
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2018-11-28 00:24:21 +0300

    JWT middleware yazildi.

---
+++ middleware/verify-token.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const token = req.headers['x-access-token'] || req.body.token || req.query.token;
+
+    if(!token)
+        return next( {message : 'Token not found'} );
+
+    if(token){
+        jwt.verify(token, req.app.get('api_secret_key'), (err, decoded) => {
+            if(err){
+                return next({message: 'Failed to auth token.'});
+            }else{
+                req.decode = decoded;
+                next();
+            }
+        });
+    }
+
+
+};
