4593655bf5a2d2da3ccb21cbb144c9e3e82432aa
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2018-11-27 20:30:15 +0300

    User route yazildi

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+// Models
+
+const User = require('../models/User');
+
+router.post('/register', (req, res, next) => {
+    const {username, password} = req.body;
+
+    bcrypt.hash(password, 10).then((hash) => {
+        const user = new User({
+            username,
+            password : hash
+        });
+
+        const promise = user.save();
+
+        promise.then(data => {
+            res.json( data );
+        }).catch( err => {
+            res.json(err);
+        });
+    });
+
+});
+
+module.exports = router;
