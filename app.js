46b510680de4fab83e498032195fae18b7a11a62
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2018-11-27 20:29:02 +0300

    User router i eklendi.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const moviesRouter = require('./routes/movies');
 const directorsRouter = require('./routes/directors');
+const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/movies', moviesRouter);
 app.use('/api/directors', directorsRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
