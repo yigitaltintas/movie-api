a558f0cad2468758de520d7fec43ba89e03d1a41
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2018-11-29 18:20:01 +0300

    Start duzenlendi

--- test/movie.test.js
+++ test/movie.test.js
@@ -61,7 +61,7 @@ describe('/api/movies test', () => {
       });
    });
 
-   describe('/GET/:director_id movie', () => {
+   describe('/GET/:movie_id movies', () => {
       it('it should GET a movie by the given id', (done) => {
          chai.request(server)
              .get('/api/movies/' + movie_id)
@@ -77,6 +77,51 @@ describe('/api/movies test', () => {
                  res.body.should.have.property('imdb_score');
                  res.body.should.have.property('_id').eql(movie_id);
                  done();
+             });
+      });
+   });
+
+   describe('/PUT/:movie_id movies', () =>{
+      it('it should UPDATE a movie given by id', (done) => {
+         const movie = {
+             title : 'Udemy Updated',
+             director_id: '5bf881a4fd96423d8f6495e2',
+             category : 'Dram Upddated',
+             country : 'Turkey Updated',
+             year : 1980,
+             imdb_score: 6
+         };
+
+         chai.request(server)
+             .put('/api/movies/' + movie_id)
+             .send(movie)
+             .set('x-access-token', token)
+             .end((err, res) => {
+                 res.should.have.status(200);
+                 res.body.should.be.a('object');
+                 res.body.should.have.property('title').eql(movie.title);
+                 res.body.should.have.property('director_id').eql(movie.director_id);
+                 res.body.should.have.property('category').eql(movie.category);
+                 res.body.should.have.property('country').eql(movie.country);
+                 res.body.should.have.property('year').eql(movie.year);
+                 res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+                 res.body.should.have.property('_id').eql(movie_id);
+                 done();
+             });
+
+      });
+   });
+
+   describe('/DELETE/:movie_id movies', () => {
+      it('it should DELETE a movie by given id', (done) => {
+         chai.request(server)
+             .delete('/api/movies/' + movie_id)
+             .set('x-access-token', token)
+             .end((err, res) => {
+                res.should.have.status(200);
+                res.should.have.be.a('object');
+                res.body.should.have.property('status').eql(1);
+                done();
              });
       });
    });
